a560019ffe23e7111e852654af9020ae524d0de1
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-02-11 22:35:03 +0500

    primeflex added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,17 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
+		provideHttpClient(),
 		provideAnimationsAsync(),
+		MessageService,
 		providePrimeNG({
 			theme: {
 				preset: Aura,
