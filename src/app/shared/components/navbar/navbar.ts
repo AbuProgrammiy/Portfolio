3acd0e7190e87256cdb83906c1a82ea415e057be
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-02-15 00:06:47 +0500

    feat(navbar): add menu to username

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { StateService } from '../../../core/services/state-service';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, Menu],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
@@ -23,6 +24,42 @@ export class Navbar {
       path: '',
     },
   ]);
+
+  protected menuItems: MenuItem[] = [
+    {
+      label: 'Options',
+      items: [
+        {
+          label: 'Telegram',
+          icon: PrimeIcons.TELEGRAM,
+          command: () => {
+            window.open('https://t.me/abuprogrammiy', '_blank');
+          },
+        },
+        {
+          label: 'Youtube',
+          icon: PrimeIcons.YOUTUBE,
+          command: () => {
+            window.open('https://www.youtube.com/@abuprogrammiy', '_blank');
+          },
+        },
+        {
+          label: 'LinkedIn',
+          icon: PrimeIcons.LINKEDIN,
+          command: () => {
+            window.open('https://www.linkedin.com/in/abu-programmiy-16b65b257', '_blank');
+          },
+        },
+        {
+          label: 'GitHub',
+          icon: PrimeIcons.GITHUB,
+          command: () => {
+            window.open('https://github.com/abuProgrammiy', '_blank');
+          },
+        },
+      ],
+    },
+  ];
 }
 
 export interface NavbarItems {
